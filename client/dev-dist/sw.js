c36de52a71e405a385bcdcd9bf8221435e26eb23
FaiazRahmanFahim <faiazrahman12.std@gmail.com>
2026-09-18 16:17:11 +0600

    feat(PWA): added pwa feature

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2168gighoq"
+    "revision": "0.upk3utajeng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
